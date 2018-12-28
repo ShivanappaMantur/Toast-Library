188885ee93f8cd7b4248c6c72ab72b3343e49e4c
Shivanappa Mantur <shivanappa.mantur@techvibrant.com>
2018-12-28 18:21:44 +0530

    Added SetToastLength Method

--- toastlib/src/main/java/com/alloneapps/toastlib/ToastMessage.java
+++ toastlib/src/main/java/com/alloneapps/toastlib/ToastMessage.java
@@ -5,7 +5,13 @@ import android.widget.Toast;
 
 public class ToastMessage {
 
+    private static int toastLengths = Toast.LENGTH_SHORT;
+
     public static void displayToast(Context context, String message){
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, message, toastLengths).show();
+    }
+
+    public static void setToastLength(int toastLength){
+        toastLengths = toastLength;
     }
 }
